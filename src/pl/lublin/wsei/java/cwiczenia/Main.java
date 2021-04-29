9485a043124feb46432473ee975dc95dc20d69a9
D4Ml4N <galkowski.damian@gmail.com>
2021-04-29 21:51:27 +0200

    Lab2/a

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -1,16 +1,10 @@
 package pl.lublin.wsei.java.cwiczenia; // deklaracja pakietu package lab1
-
 import pl.lublin.wsei.java.cwiczenia.mylib.Account;
-
-
 
 public class Main {
 
     public static void main(String[] args) {
-        Account acc = new Account();
-        acc.setName("piotr Gołabek");
-        System.out.println(acc.getName());
-        System.out.printf("%s %n", Account.translit("дамиан галковски"));
+        Account.main();
     }
 }
 
